e9f20894f2bd73b8dc8751cb8cbee4ec4950c094
fernando.vendramin <fernando.vendramin@dextra-sw.com>
2021-10-11 18:17:48 -0300

    ajuste appsettings

--- jogajunto-frontend/jogajunto-frontend/AppSettings.cs
+++ jogajunto-frontend/jogajunto-frontend/AppSettings.cs
@@ -4,8 +4,6 @@
     {
         public static string JogaJuntoApiUrl { get => "https://us-central1-joga-junto-app.cloudfunctions.net/"; }
 
-        public static string JogaJuntoHome { get => $"{JogaJuntoApiUrl}api/home"; }
-
         public static string OffLineDataBaseName { get => "adimaxpro.db"; }
     }
 }
